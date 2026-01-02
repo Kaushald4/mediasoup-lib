872bb5d76cab0d57193dd84ef45bfadfea517af2
Kaushal <kaushalkumar304@gmail.com>
2026-01-03 00:42:47 +0530

    feat: Implement hooks for managing connection state, data channels, media devices, and participants

--- packages/client/src/hooks/index.ts
+++ packages/client/src/hooks/index.ts
@@ -2,5 +2,23 @@
  * React hooks for mediasoup-lib client
  */
 
-// Placeholder - hooks will be implemented in Phase 12
-export {};
+export { useLocalParticipant } from './useLocalParticipant';
+export { useParticipants, useParticipant, useParticipantByIdentity } from './useParticipants';
+export {
+  useTracks,
+  useTracksByKind,
+  useAudioTracks,
+  useVideoTracks,
+  useTrackPublications,
+  useTrackPublicationsByKind,
+  useLocalTracks,
+} from './useTracks';
+export {
+  useConnectionState,
+  useIsConnected,
+  useIsConnecting,
+  useIsDisconnected,
+  useIsReconnecting,
+} from './useConnectionState';
+export { useDataChannel, useDataChannelListener } from './useDataChannel';
+export { useMediaDevices } from './useMediaDevices';
